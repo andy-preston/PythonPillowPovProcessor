6515002ff01b54670d5432020dd53dcfa797f559
Andy Preston <edgeeffect@gmail.com>
2023-07-08 13:28:28 +0100

    tidy up stream_output

--- imports/stream_output.py
+++ imports/stream_output.py
@@ -1,7 +1,35 @@
-"""Module for stream output class"""
+"""Module for stream output classes"""
 import subprocess
 from typing import Tuple, Dict, Callable
 import ffmpeg
+
+
+class OutputSequence:
+    """Class to handle outputting in chunks rather than 1 big file"""
+
+    _file_parts: Tuple
+    _file_index: int
+    _frame_count: int
+    _a_minute: int
+
+    def __init__(self, file_template: str, frame_rate: int, output_seconds: int):
+        self._file_parts = tuple(file_template.split("."))
+        self._file_index = 0
+        self._frame_count = -1
+        self._a_minute = frame_rate * output_seconds
+
+    def new_file_required(self) -> bool:
+        """check to see if a new file is required"""
+        self._frame_count = self._frame_count + 1
+        result = self._frame_count > self._a_minute or self._frame_count < 1
+        if result:
+            self._frame_count = 0
+        return result
+
+    def filename(self):
+        """return the next filename in the sequence"""
+        self._file_index = self._file_index + 1
+        return self._file_parts[0] + f"-{self._file_index:05d}." + self._file_parts[1]
 
 
 class OutputStream:
@@ -10,38 +38,30 @@ class OutputStream:
     _process: subprocess.Popen
     _buffer_size: int
     _size_string: str
-    _frame_count: int
-    _a_minute: int
-    _file_parts: Tuple
-    _file_index: int
     _finished: Callable
     _filename: str
     _config: Dict
+    _output_sequence: OutputSequence
 
     def __init__(self, config: Dict, attributes: Dict, finished: Callable):
         self._config = config
         self._set_attributes(attributes)
-        self._file_list(config["output_template"])
         self._finished = finished
         self._process = None
 
     def _set_attributes(self, attributes: Dict):
         self._size_string = f"{attributes['width']}x{attributes['height']}"
         self._buffer_size = attributes["buffer_size"]
-        self._a_minute = attributes["frame_rate"] * self._config["output_seconds"]
-        self._frame_count = 0
-
-    def _file_list(self, base: str):
-        self._file_parts = tuple(base.split("."))
-        self._file_index = 0
+        self._output_sequence = OutputSequence(
+            self._config["output_template"],
+            attributes["frame_rate"],
+            self._config["output_seconds"],
+        )
 
     def _open(self):
         """open the next stream in the sequence"""
         self.close()
-        self._file_index = self._file_index + 1
-        self._filename = (
-            self._file_parts[0] + f"-{self._file_index:05d}." + self._file_parts[1]
-        )
+        self._filename = self._output_sequence.filename()
         args = (
             ffmpeg.input(
                 "pipe:", format="rawvideo", pix_fmt="rgb24", s=self._size_string
@@ -54,10 +74,8 @@ class OutputStream:
 
     def write(self, frame_bytes: bytes):
         """write a frame to the stream"""
-        if self._frame_count >= self._a_minute or self._process is None:
+        if self._output_sequence.new_file_required():
             self._open()
-            self._frame_count = 0
-        self._frame_count = self._frame_count + 1
         self._process.stdin.write(frame_bytes)
 
     def close(self):
